db15a3b51bbd51d01fe926676e4519427dcca2ce
ilia <ilyakut@gmail.com>
2020-08-25 23:39:19 +0300

    added some style

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 importScripts('/src/js/idb.js'); // метод для импорта скриптов из проекта в SW
 importScripts('/src/js/utility.js');
 
-const CACHE_STATIC_NAME = 'static-v15';
+const CACHE_STATIC_NAME = 'static-v21';
 const CACHE_DYNAMIC_NAME = 'dynamic-v2'
 const STATIC_FILES = [
   '/', //запрос по умолчанию тоже нужно кэшировать
